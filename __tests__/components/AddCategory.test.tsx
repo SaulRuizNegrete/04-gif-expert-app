3171b0b95b29e605ddcb740d505a3bc8604248c7
SaulRuizNegrete <saul53141@gmail.com>
2023-09-27 10:23:18 -0600

    Pruebas al AddCategory component

--- __tests__/components/AddCategory.test.tsx
+++ __tests__/components/AddCategory.test.tsx
@@ -2,15 +2,47 @@ import React from "react"
 import { render, screen, fireEvent } from "@testing-library/react"
 import { AddCategory } from '../../src/components'
 
-describe('pruebas en <AddCategory />', () => { 
-    test('debe cambiar el valor de una caja de texto', () => { 
+describe('pruebas en <AddCategory />', () => {
+    test('debe cambiar el valor de una caja de texto', () => {
 
-        render(<AddCategory onNewCategory={ () => {} } />)
+        render(<AddCategory onNewCategory={() => { }} />)
         const input = screen.getByRole('textbox') as HTMLInputElement
 
-        fireEvent.input( input, { target: { value: 'Saitama'}} )
+        fireEvent.input(input, { target: { value: 'Saitama' } })
 
-        expect( input.value ).toBe('Saitama')
+        expect(input.value).toBe('Saitama')
 
-     })
- })
+    })
+
+    test('debe de llamar onNewCategory si el input tiene un valor', () => {
+
+        const inputValue = 'Saitama'
+        const onNewCategory = jest.fn()
+
+        render(<AddCategory onNewCategory={onNewCategory} />)
+
+        const input = screen.getByRole('textbox') as HTMLInputElement
+        const form = screen.getByRole('form')
+
+        fireEvent.input(input, { target: { value: inputValue } })
+        fireEvent.submit(form)
+
+        expect(input.value).toBe('')
+        expect(onNewCategory).toHaveBeenCalled()
+        expect(onNewCategory).toHaveBeenCalledTimes(1)
+        expect(onNewCategory).toHaveBeenCalledWith(inputValue)
+
+    })
+
+    test('no debe de llamar el onNewCategory si el input está vacío', () => {
+        const onNewCategory = jest.fn()
+
+        render(<AddCategory onNewCategory={onNewCategory} />)
+
+        const form = screen.getByRole('form')
+        fireEvent.submit(form)
+
+        expect(onNewCategory).toHaveBeenCalledTimes(0)
+        //expect(onNewCategory).not.toHaveBeenCalled()
+    })
+})
--- src/components/AddCategory.tsx
+++ src/components/AddCategory.tsx
@@ -18,7 +18,7 @@ export const AddCategory = ({ onNewCategory }: any) => {
     }
 
     return (
-        <form onSubmit={ (event) => onSubmit(event) }>
+        <form onSubmit={ (event) => onSubmit(event) } aria-label='form'>
             <input 
                 type="text" 
                 placeholder="Buscar gifs"
